0a2354eb925dcbe35ef8131a78c646583e003ae1
Grellashbk12 <armando.briseno@alumnos.udg.mx>
2024-05-28 20:02:07 -0600

    tercer commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-console.log("Hola Mundo")
+function recogeDatos (){
+    var nombre = document.querySelector("#nombre").value;
+    var anio = document.querySelector("#year").value;
+
+    var edad = 2024 - anio;
+
+    console.log("Hola tu nombre es: " + nombre +  " tu año de nacimiento: " + anio + " y tu edad: " + edad   )
+}
+
+
+recogeDatos();
